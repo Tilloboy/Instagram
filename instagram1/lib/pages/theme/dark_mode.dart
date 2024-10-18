d8fe5301348de082b29eb289d1334b3a5c2736e4
Tilloboy <tilloboy1202@gmail.com>
2024-10-18 04:52:17 -0700

    light mood va dark moodni qushdm

---
+++ instagram1/lib/pages/theme/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData dark_mode = ThemeData(
+    colorScheme: ColorScheme.dark(
+  background: Colors.grey.shade600,
+  primary: Colors.grey.shade600,
+  secondary: Colors.grey.shade800,
+  inversePrimary: Colors.grey.shade300,
+
+  
+));
